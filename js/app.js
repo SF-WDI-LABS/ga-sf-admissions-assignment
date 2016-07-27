53f9c29d1fdbd90e9eeacb7c0347ba56d7927449
Ilias Tsangaris <iliastsangaris@gmail.com>
2016-07-27 13:47:56 -0700

    adds assets

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// message in the console
+console.log("TYPE 'app.help()' FOR INSTRUCTIONS");
+
+// initialize the application
+var app = new Object();
+
+// define methods on the appliction
+app.help = function() {
+  // show instructions
+}
