c643b291b476778b012a8a11757ddf46b546d075
Ilias Tsangaris <iliastsangaris@gmail.com>
2016-07-28 17:09:41 -0700

    scaffolds lesson

--- js/app.js
+++ js/app.js
@@ -5,3 +5,14 @@ console.log("Hi there, type 'app' to see the methods available to the applicatio
 var app = new Object();
 
 // define methods on the appliction
+app.setName = function() {
+  // 
+}
+
+app.setHobby = function() {
+  //
+}
+
+app.setWebsite = function() {
+  //
+}
