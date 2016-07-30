a5f6b6a54312bfacc8f93ab327b5e605453a0b47
Ilias Tsangaris <iliastsangaris@gmail.com>
2016-07-29 18:16:02 -0700

    adds assets & vendor directories

---
+++ assets/js/app.js
@@ -0,0 +1,18 @@
+// message in the console
+console.log("Hi there, welcome to the JavaScript Console. Feel free to type 'app' to see the methods available to the application we're going to build.");
+
+// initialize the application
+var app = new Object();
+
+// define methods on the appliction
+app.setName = function setName() {
+
+}
+
+app.setHobby = function setHobby() {
+
+}
+
+app.setWebsite = function setWebsite() {
+
+}
