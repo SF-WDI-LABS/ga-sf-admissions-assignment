b4907f463188b01204700be771e6e02194ee25f9
Ilias Tsangaris <iliastsangaris@gmail.com>
2016-08-22 21:05:11 -0700

    standardizes challenges language

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,22 +4,17 @@ console.log("Hi there, welcome to the JavaScript console!");
 // initialize the application
 var app = new Object();
 
-// define methods on the appliction
-app.setName = function setName() {
-  console.log("executing function app.setName"); // sanity check
-  /*
-    code here
-    Step 1: prompt the user for their name
-    Step 2: target the element with id "first-name"
-    Step 3: replace its inner HTML of the element with the name of the user
-  */
-}
+app.shakePokeball = function shakePokeball() {
+  // code BELOW this line for challenge #1 -->
 
-app.setHobby = function setHobby() {
-  console.log("executing function app.setHobby"); // sanity check
-  // code here
-}
+} // <-- code ABOVE this line for challenge #1
+
+app.setName = function setName() {
+  // code BELOW this line for challenge #2 -->
+
+} // <-- code ABOVE this line for challenge #2
 
 app.setWebsite = function setWebsite() {
-  // code here
-}
+  // code BELOW this line for challenge #3 & bonus -->
+
+} // <-- code ABOVE this line for challenge #3 & bonus
